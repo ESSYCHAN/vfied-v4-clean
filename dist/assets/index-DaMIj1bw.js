29d9833fbe2b95a876cec3bb8f479b7fdff51659
Esther Mulwa <mulwa.esther@gmail.com>
2025-09-28 21:34:29 +0100

    feat: successful iOS mobile deployment with working UI

--- dist/assets/index-DaMIj1bw.js
+++ dist/assets/index-DaMIj1bw.js
@@ -1,2 +1,2 @@
 function t(){import.meta.url,import("_").catch(()=>1),(async function*(){})().next()}export{t as __vite_legacy_guard};
-//# sourceMappingURL=index-WkmtJsBs.js.map
+//# sourceMappingURL=index-DaMIj1bw.js.map
